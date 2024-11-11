8d731fcaa245f170e5dd6bc7c0cced11c5ca0dd8
Shiva <shivanandhan33@gmail.com>
2024-11-11 22:00:50 +0530

    ec2 creation

--- main.tf
+++ main.tf
@@ -3,7 +3,3 @@ resource "aws_instance" "frontend" {
   instance_type          = "t3.small"
 }
 
-provider "aws" {
-  region  = "us-west-2"
-  profile = "terraform-user"
-}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAWFIPSTEZEJKG4HOC"
+  secret_key = "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"
+}
