382e6841959a5c07a92244825f979211adb40343
Shiva <shivanandhan33@gmail.com>
2024-11-11 20:48:38 +0530

    ec2 creation

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,9 +1,4 @@
 resource "aws_instance" "frontend" {
   ami                    = "ami-041e2ea9402c46c32"
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-065fe091c4525f4dd"]
-
-  tags = {
-    Name = "frontend.dev"
-  }
 }
