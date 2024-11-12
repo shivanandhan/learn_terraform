0793c885cd711dace3c19ff2183979bcac484775
Shiva <shivanandhan33@gmail.com>
2024-11-12 22:04:12 +0530

    ec2 creation1

--- data/main.tf
+++ data/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "frontend" {
 data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "RHEL-9-DevOps-Practice"
-  owners           = "973714476881"
+  owners           = ["973714476881"]
 }
 
 data "aws_security_group" "sg" {
